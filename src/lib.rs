ba103a9cd6e85ac18c707db966404718ebc0516a
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-04-11 21:58:14 +0000

    fixing

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,23 @@
 #[allow(dead_code)]
-pub mod coord;
-pub use coord::Coord;
-
+pub mod coords;
+pub mod lines;
 
 #[test]
 #[cfg(test)]
 fn test_coords () {
 
-    let a: Coord = coord::new(1, 2);
+    let a: coords::Coord = coords::new(1, 2);
     let (x , y) = (3, 3);
-    let b: Coord = coord::new(x, y);
+    let b: coords::Coord = coords::new(x, y);
     
     // math
-    let c: Coord = a + b;
-    let d: Coord = a - c;
-    let e: Coord = a * d;
-    let f: Coord = a / a;
+    let c: coords::Coord = a + b;
+    let d: coords::Coord = a - c;
+    let e: coords::Coord = a * d;
+    let f: coords::Coord = a / a;
     
     // to vec
-    let g: Vec<i32> = coord::Coord::to_vec(a);
+    let g: Vec<i32> = coords::Coord::to_vec(a);
     
     println!("let a: coord::Coord = coord::new(1, 2); | {:?}", a);
     println!("\nlet (x , y) = (3, 3);\nlet b: coord::Coord = coord::new(x, y); | {:?}", b);
@@ -27,5 +26,6 @@ fn test_coords () {
     println!("\nlet e: coord::Coord = a * d; | {:?}", e);
     println!("\nlet f: coord::Coord = a / a; | {:?}", f);
     println!("\nlet g: Vec<i32> = coord::Coord::to_vec(a); | {:?}", g);
+
 
 }
